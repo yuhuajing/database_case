52ca6963caadbe2a544c522f805e19ad1c7ea9f6
yuhuajing <18852897525@163.com>
2024-05-28 11:38:17 +0800

    update

--- main.go
+++ main.go
@@ -2,117 +2,83 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"github.com/go-redis/redis/v8"
+	"github.com/joho/godotenv"
+	"os"
 	"time"
-
-	log "github.com/sirupsen/logrus"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-const (
-	password = "password"
-	dbname   = "clayte"
-	username = "clay"
-	//transfer_collections = "transfer"
-	collections = "clayte"
-)
+var client *redis.Client
 
-type User struct {
-	Name     string `json:"name"`
-	Sex      string `json:"sex"`
-	Interest string `json:"interest"`
-	Age      int    `json:"age"`
-	//Token     string `json:"token"`
+func init() {
+	loadenv()
+	connredis()
+}
+func connredis() {
+	rpc := Env("REDIS_HOSTS", "")
+	client = redis.NewClient(&redis.Options{Addr: rpc})
+	_, err := client.Ping(context.Background()).Result()
+	if err != nil {
+		fmt.Printf("Redis链接失败：%s\n", err)
+		return
+	}
+	//fmt.Println(pong)
+}
+func loadenv() {
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	} // The Original .env
+	_ = godotenv.Overload(".env")
 }
 
-type ResponseResult struct {
-	ResponseCode int    `json:"rc"`
-	Error        string `json:"error"`
-	Result       string `json:"result"`
-}
-
-func GetDBCollection() (*mongo.Collection, error) {
-	log.SetFormatter(&log.JSONFormatter{})
-	// client, err := mongo.NewClient((options.Client().ApplyURI("mongodb://localhost:27017")))
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI(
-		"mongodb://"+username+":"+password+"@localhost:27017",
-		//"mongodb+srv://standard:"+password+"@cluster0.pdpui.mongodb.net/"+dbname+"?retryWrites=true&w=majority",
-	))
-
-	if err != nil {
-		log.Error(err.Error())
-		return nil, err
+func Env(key string, d string) string {
+	env, ok := os.LookupEnv(key)
+	if ok {
+		return env
 	}
 
-	//Check the connection
-	err = client.Ping(context.TODO(), nil)
-	if err != nil {
-		log.Error(err.Error())
-		return nil, err
-	}
-
-	collection := client.Database(dbname).Collection(collections)
-	return collection, nil
-}
-
-func insert_after_query(collection *mongo.Collection) bool {
-	var result User = User{Name: "clayte", Sex: "man", Interest: "Baseball", Age: 28}
-	var res User
-	err := collection.FindOne(context.TODO(), bson.D{{Key: "name", Value: result.Name}}).Decode(&res)
-	if err != nil {
-		_, err = collection.InsertOne(context.TODO(), bson.D{{Key: "name", Value: result.Name}, {Key: "sex", Value: result.Sex}, {Key: "age", Value: result.Age}})
-		if err != nil {
-			return false
-		}
-	}
-	return true
-}
-
-func query_single(collection *mongo.Collection) bool {
-	var result User = User{Name: "clayer", Sex: "man"}
-	filter := bson.D{{Key: "name", Value: result.Name}, {Key: "sex", Value: result.Sex}}
-	opts := options.Find().SetSort(bson.D{{Key: "age", Value: -1}}).SetLimit(1)
-	cur, err := collection.Find(context.TODO(), filter, opts)
-	if err != nil {
-		log.Fatal(err)
-	}
-	var results []User
-
-	if err = cur.All(context.Background(), &results); err != nil {
-		log.Fatal(err)
-	}
-
-	for _, val := range results {
-		fmt.Println(val)
-	}
-	return true
-}
-
-func update(collection *mongo.Collection) bool {
-	var result User = User{Name: "clayer", Sex: "man", Interest: "Table"}
-	filter := bson.D{{Key: "name", Value: result.Name}, {Key: "sex", Value: result.Sex}}
-	update := bson.D{{Key: "$set", Value: bson.D{{Key: "interest", Value: result.Interest}}}}
-
-	updateres, err := collection.UpdateOne(context.TODO(), filter, update)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(updateres.ModifiedCount)
-	return true
+	return d
 }
 
 func main() {
-	collection, err := GetDBCollection()
+	key := "Alice"
+	expiration := 15 * time.Second
+	//value := "Elison"
+	value, err := json.Marshal(Auther{Name: key, Age: 29})
 	if err != nil {
+		fmt.Printf("Json Marshal 编码数据错误：%s\n", err)
 		return
 	}
-	//	insert_after_query(collection)
-	//query_single(collection)
-	update(collection)
+	err = setvalue(client, key, string(value), expiration)
+	if err != nil {
+		fmt.Printf("Redis Set键值数据错误：%s\n", err)
+		return
+	}
+	values, err := getvalue(client, key)
+	if err != nil {
+		fmt.Printf("Redis Get键值数据错误：%s\n", err)
+		return
+	}
+	valuestr := fmt.Sprintf("%s", values)
+	var author Auther
+	json.Unmarshal([]byte(valuestr), &author)
+	fmt.Println(values)
+}
 
-	fmt.Println("123")
+func setvalue(c *redis.Client, key string, value interface{}, expiration time.Duration) error {
+	err := c.Set(context.Background(), key, value, expiration).Err()
+	return err
+}
+
+func getvalue(c *redis.Client, key string) (interface{}, error) {
+	val, err := c.Get(context.Background(), key).Result()
+	return val, err
+}
+
+type Auther struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
 }
